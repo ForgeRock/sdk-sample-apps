b8444b40b5e4f193d7b10d1112d307482cec08f0
ryanbas21 <ryan.basmajian@forgerock.com>
2023-11-20 13:12:45 -0700

    retrack angular

---
+++ angular-todo/set-env.mjs
@@ -0,0 +1,29 @@
+import { writeFile } from 'fs';
+import dotenv from 'dotenv';
+dotenv.config();
+
+// Assume development
+let targetPath =
+  process.env.NODE_ENV === 'production'
+    ? 'src/environments/environment.prod.ts'
+    : 'src/environments/environment.ts';
+
+const envConfigFile = `export const environment = {
+   AM_URL: '${process.env.AM_URL}',
+   REALM_PATH: '${process.env.REALM_PATH}',
+   WEB_OAUTH_CLIENT: '${process.env.WEB_OAUTH_CLIENT}',
+   JOURNEY_LOGIN: '${process.env.JOURNEY_LOGIN}',
+   JOURNEY_REGISTER: '${process.env.JOURNEY_REGISTER}',
+   API_URL: '${process.env.API_URL}',
+   production: '${process.env.NODE_ENV || 'development'}'
+};
+`;
+console.log(`The file ${targetPath} will be written with the following content: \n`);
+console.log(envConfigFile);
+writeFile(targetPath, envConfigFile, function (err) {
+  if (err) {
+    throw console.error(err);
+  } else {
+    console.log(`Angular environment file generated correctly at ${targetPath} \n`);
+  }
+});
