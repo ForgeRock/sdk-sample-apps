b1d498facd26edf83613e5c54501be9536a5d063
ryanbas21 <ryanbas21@gmail.com>
2024-09-16 09:33:23 -0600

    chore: reorganize-samples

---
+++ JS/angular-todo/src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+   AM_URL: 'https://openam-sdks.forgeblocks.com/am',
+   REALM_PATH: 'alpha',
+   WEB_OAUTH_CLIENT: 'CentralLoginOAuthClient-',
+   JOURNEY_LOGIN: 'Login',
+   JOURNEY_REGISTER: 'Registration',
+   API_URL: 'http://localhost:9443',
+   production: 'development',
+   CENTRALIZED_LOGIN: 'false'
+};
