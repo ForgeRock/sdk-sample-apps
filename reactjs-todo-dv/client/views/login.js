0a1a333c4db8a8074d215a5465e6b9eafa6f26b0
jon-oblander <jonoblander@pingidentity.com>
2024-11-06 09:47:53 -0800

    refactor updates

--- reactjs-todo-dv/client/views/login.js
+++ reactjs-todo-dv/client/views/login.js
@@ -10,6 +10,7 @@
 import React from 'react';
 import BackHome from '../components/utilities/back-home';
 import Card from '../components/layout/card';
+import { useNavigate } from 'react-router-dom';
 import DaVinciFlow from '../components/davinci-client/davinci-flow.js';
 import { CLIENT_ID, REDIRECT_URI, SCOPE, BASE_URL } from '../constants';
 
@@ -18,6 +19,9 @@ import { CLIENT_ID, REDIRECT_URI, SCOPE, BASE_URL } from '../constants';
  * @returns {Object} - React component object
  */
 export default function Login() {
+  // Used for redirection after success
+  const navigate = useNavigate();
+
   // Set DaVinci config object
   const config = {
     clientId: CLIENT_ID, // oAuth client id
@@ -29,12 +33,18 @@ export default function Login() {
     },
   };
 
+  // This function is passed to DaVinciFlow to be called when the flow is complete
+  // Each individual flow might have a different completion action
+  function onComplete() {
+    navigate('/');
+  }
+
   return (
     <div className="cstm_container_v-centered container-fluid d-flex align-items-center">
       <div className="w-100">
         <BackHome />
         <Card>
-          <DaVinciFlow config={config} />
+          <DaVinciFlow config={config} flowCompleteCb={onComplete} />
         </Card>
       </div>
     </div>
