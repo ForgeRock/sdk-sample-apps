054d8666cafc7d3cefc2ee11c6ff83f8971aa817
jon-oblander <jonoblander@pingidentity.com>
2024-11-13 09:24:10 -0800

    duplicate existing angualr app. no code changes

---
+++ angular-todo-dv/e2e/social-login.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test';
+
+test('Angular - Login with social login', async ({ page }) => {
+  await page.goto('https://localhost:8443/home?journey=TEST_LoginWithSocial');
+  await page.getByRole('link', { name: 'Sign In', exact: true }).click();
+
+  await expect(page.getByLabel('User Name')).toBeEmpty();
+  await expect(page.getByLabel('User Name')).toBeVisible();
+  await expect(page.getByLabel('Password')).toBeEmpty();
+  await expect(page.getByLabel('Password')).toBeVisible();
+
+  await expect(page.getByRole('button', { name: 'Sign in with Apple' })).toBeVisible();
+  await expect(page.getByRole('button', { name: 'Sign in with Facebook' })).toBeVisible();
+  await expect(page.getByRole('button', { name: 'Sign in with Google' })).toBeVisible();
+});
---
+++ angular-todo-dv/playwright.config.ts
@@ -0,0 +1,56 @@
+import { defineConfig, devices } from '@playwright/test';
+
+const url = process.env.PLAYWRIGHT_TEST_BASE_URL || 'https://localhost:8443';
+
+export default defineConfig({
+  testDir: 'e2e',
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: 'html',
+  use: {
+    trace: 'on',
+    baseURL: `${url}`,
+    ignoreHTTPSErrors: true,
+    headless: !!process.env.CI,
+  },
+  webServer: [
+    {
+      command: 'npm run start:angular-todo',
+      url,
+      timeout: 120 * 1000,
+      reuseExistingServer: !process.env.CI,
+      cwd: '../',
+      env: {
+        API_URL: 'http://localhost:9443',
+        DEBUGGER_OFF: 'true',
+        DEVELOPMENT: 'false',
+        JOURNEY_LOGIN: 'Login',
+        JOURNEY_REGISTER: 'Registration',
+        PORT: '9443',
+        AM_URL: 'https://openam-sdks.forgeblocks.com/am',
+        REALM_PATH: 'alpha',
+        SCOPE: 'profile me.read email',
+        TIMEOUT: '3000',
+        WEB_OAUTH_CLIENT: 'CentralLoginOAuthClient-',
+        REST_OAUTH_CLIENT: 'RestOAuthClient',
+        REST_OAUTH_SECRET: process.env.REST_OAUTH_SECRET as string,
+        CENTRALIZED_LOGIN: 'false',
+      },
+      ignoreHTTPSErrors: true,
+    },
+  ],
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'firefox',
+      grepInvert: /WebAuthN/i,
+      use: { ...devices['Desktop Firefox'] },
+    },
+  ],
+});
