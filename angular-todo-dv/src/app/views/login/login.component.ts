2199128a4bba0070aeaf01689cfcba054efcd937
jon-oblander <jonoblander@pingidentity.com>
2024-11-13 10:37:32 -0800

    additional app updates

--- angular-todo-dv/src/app/views/login/login.component.ts
+++ angular-todo-dv/src/app/views/login/login.component.ts
@@ -9,7 +9,6 @@
  */
 
 import { Component, OnInit } from '@angular/core';
-import { environment } from '../../../environments/environment';
 import { TokenManager, UserManager } from '@forgerock/javascript-sdk';
 import { ActivatedRoute, Router } from '@angular/router';
 import { UserService } from '../../services/user.service';
@@ -30,10 +29,6 @@ export class LoginComponent implements OnInit {
   loadingMessage: string;
   journey: string;
 
-  get isCentralizedLogin(): boolean {
-    return this.centralLogin === 'true' || environment.CENTRALIZED_LOGIN === 'true' ? true : false;
-  }
-
   constructor(
     private route: ActivatedRoute,
     public userService: UserService,
@@ -46,38 +41,14 @@ export class LoginComponent implements OnInit {
     this.state = this.route.snapshot.queryParamMap.get('state');
     this.centralLogin = this.route.snapshot.queryParamMap.get('centralLogin');
     this.journey = this.route.snapshot.queryParamMap.get('journey');
-
-    if (this.isCentralizedLogin) {
-      if (this.code && this.state) {
-        /** *****************************************************************
-         * SDK INTEGRATION POINT
-         * Summary: Get OAuth/OIDC tokens with Authorization Code Flow w/PKCE.
-         * ------------------------------------------------------------------
-         * Details:When the user return to this app after successfully logging in,
-         * the URL will include code and state query parameters that need to
-         * be passed in to complete the OAuth flow giving the user access
-         ***************************************************************** */
-        this.loadingMessage = 'Success! Redirecting ...';
-        await this.authorize(this.code, this.state);
-      } else if (this.error) {
-        // Do nothing as this will result in a redirect
-      } else {
-        /** *****************************************************************
-         * SDK INTEGRATION POINT
-         * Summary: Get OAuth/OIDC tokens with Authorization Code Flow w/PKCE.
-         * ------------------------------------------------------------------
-         * The key-value of `login: redirect` is what allows central-login.
-         * Passing no arguments or a key-value of `login: 'embedded'` means
-         * the app handles authentication locally
-         ***************************************************************** */
-        this.loadingMessage = 'Redirecting ...';
-        await TokenManager.getTokens({ login: 'redirect', query: { acr_values: 'simpleLogin' } });
-      }
-    }
   }
 
   onSetIsWebAuthn(isWebAuthn: boolean): void {
     this.isWebAuthn = isWebAuthn;
+  }
+
+  onCompleteFlow(): void {
+    this.router.navigateByUrl('/');
   }
 
   async authorize(code: string, state: string): Promise<void> {
