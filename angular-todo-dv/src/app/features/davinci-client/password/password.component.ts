9912b4bc065c301fb0be9dcdb7bd472aad920f04
jon-oblander <jonoblander@pingidentity.com>
2024-11-13 09:39:49 -0800

    add in davinci client components

---
+++ angular-todo-dv/src/app/features/davinci-client/password/password.component.ts
@@ -0,0 +1,31 @@
+/*
+ * angular-todo-prototype
+ *
+ * password.component.ts
+ *
+ * Copyright (c) 2021 ForgeRock. All rights reserved.
+ * This software may be modified and distributed under the terms
+ * of the MIT license. See the LICENSE file for details.
+ */
+
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-password',
+  templateUrl: './password.component.html',
+})
+export class PasswordComponent {
+  @Input() key: string;
+  @Input() label: string;
+  @Output() valueUpdated = new EventEmitter<string>();
+
+  isVisible = false;
+
+  toggleVisibility(): void {
+    this.isVisible = !this.isVisible;
+  }
+
+  updateValue(event: Event): void {
+    this.valueUpdated.emit((event.target as HTMLInputElement).value);
+  }
+}
