aa4797fd2118d7b446ec4fcecbdce24bf3391062
jey <jey.periyasamy@forgerock.com>
2024-12-04 13:56:26 -0600

    adding slowing server component

---
+++ reactnative/reactnativetodo/.env.js
@@ -0,0 +1,20 @@
+/**
+ * Avoid trailing slashes in the URL string values below
+ */
+const AM_URL = 'https://openam-forgerrock-sdks.forgeblocks.com/am';
+const API_PORT = 8080;
+const API_BASE_URL = 'http://localhost';
+const DEBUGGER_OFF = false;
+const REALM_PATH = 'alpha';
+const REST_OAUTH_CLIENT = 'RestOAuthClient';
+const REST_OAUTH_SECRET = 'sf0h8e350efhd932';
+
+export {
+  AM_URL,
+  API_BASE_URL,
+  API_PORT,
+  DEBUGGER_OFF,
+  REALM_PATH,
+  REST_OAUTH_CLIENT,
+  REST_OAUTH_SECRET,
+};
