f6de62289ba37f2c64733dab24a092e58e3ece5a
AJ Ancheta <7781450+ancheetah@users.noreply.github.com>
2025-04-18 14:02:10 -0400

    move angular davinci sample app

---
+++ javascript/angular-todo-davinci/src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  API_URL: 'undefined',
+  DEBUGGER_OFF: 'undefined',
+  DEVELOPMENT: 'undefined',
+  PORT: 'undefined',
+  CLIENT_ID: 'undefined',
+  REDIRECT_URI: 'undefined',
+  SCOPE: 'undefined',
+  BASE_URL: 'undefined',
+  production: 'development'
+};
