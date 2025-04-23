81598b65d70554da054ae7c4af5ac89f165d6c9e
AJ Ancheta <7781450+ancheetah@users.noreply.github.com>
2025-04-23 16:00:15 -0400

    chore(angular-todo-davinci): update copyright

--- javascript/angular-todo-davinci/src/app/auth/auth.guard.ts
+++ javascript/angular-todo-davinci/src/app/auth/auth.guard.ts
@@ -1,10 +1,10 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /*
- * angular-todo-prototype
+ * ping-sample-web-angular-davinci
  *
  * auth.guard.ts
  *
- * Copyright (c) 2021 ForgeRock. All rights reserved.
+ * Copyright (c) 2025 Ping Identity Corporation. All rights reserved.
  * This software may be modified and distributed under the terms
  * of the MIT license. See the LICENSE file for details.
  */
--- javascript/angular-todo-davinci/src/app/shared/idps.types.ts
+++ javascript/angular-todo-davinci/src/app/shared/idps.types.ts
@@ -1,9 +1,9 @@
 /*
- * @forgerock/javascript-sdk
+ * ping-sample-web-angular-davinci
  *
  * idps.types.ts
  *
- * Copyright (c) 2020 ForgeRock. All rights reserved.
+ * Copyright (c) 2025 Ping Identity Corporation. All rights reserved.
  * This software may be modified and distributed under the terms
  * of the MIT license. See the LICENSE file for details.
  */
