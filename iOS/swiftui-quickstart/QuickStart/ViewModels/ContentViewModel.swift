5c4fb691952f8f3a265d59b8d61ea3706f45ab1d
George Bafaloukas <george.bafaloukas@forgerock.com>
2025-06-05 19:31:08 +0300

    iOS swift-ui quickstart udpate to work with FROptions initialization and some extra commens

--- iOS/swiftui-quickstart/QuickStart/ViewModels/ContentViewModel.swift
+++ iOS/swiftui-quickstart/QuickStart/ViewModels/ContentViewModel.swift
@@ -19,9 +19,38 @@ import FRAuth
         
         do {
             // By default the SDK uses FRAuthConfig.plist file for the configuration.
-            // You can also specify the desired config file like this: `FRAuth.configPlistFileName = "DesiredFRAuthConfig"`
-            // You can also specify the configurations dynamically by using FROptions. Please see the docs for more details here: https://docs.pingidentity.com/sdks/latest/ios/configuring/dynamic-configuration.html
-            try FRAuth.start()
+            // You can also use an FROptions object to dynamically configure the SDK.
+            // In this project we will be using the FROptions route. If insted you want to use the FRAuthConfig.plist config file,
+            // you can edit the provided FRAuthConfig.plist file in the Resources folder.
+            // Calling `try FRAuth.start()` will load the configuration from the default FRAuthConfig.plist file.
+            // You can also specify a separate file like this: `FRAuth.configPlistFileName = "DesiredFRAuthConfig"`
+            // Please see the docs for more details here: https://docs.pingidentity.com/sdks/latest/sdks/sdkconfiguration/configure-sdk-ios.html#example
+            let options = FROptions(
+                // The tenant URL
+                url: "https://tenant.forgeblocks.com/am",
+                // Realm Name
+                realm: "alpha",
+                // The cookie name for the tenant
+                cookieName: "46b42b4229cd7a3",
+                // The values below are the default journeys in your tenant.
+                // Modify as needed.
+                // Uses default Login journey for sample app.
+                // Updated as needed based on the journey you create.
+                authServiceName: "Login",
+                // Uses default Registration journey for sample app.
+                // Updated as needed based on the journey you create.
+                registrationServiceName: "Register",
+                // Client ID for the OAuth2 client.
+                oauthClientId: "sdkNativeClient",
+                // Redirect URI back to client, update
+                // if you have another URI to add.
+                oauthRedirectUri: "org.forgerock.demo://oauth2redirect",
+                // By default, all scopes configured in the client are added,
+                // modify as needed.
+                oauthScope: "openid profile email address")
+            // Replace the above values with your actual configuration values.
+            try FRAuth.start(options: options)
+            
             print("SDK initialized successfully")
             statusViewModel.status = Status(statusDescription: "SDK ready", statusType: .success)
             sdkStarted = true
