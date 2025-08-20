c04278d72f6a482ffa6d2c558863c96826b725a7
AJ Ancheta <7781450+ancheetah@users.noreply.github.com>
2025-08-20 16:16:35 -0400

    test(central-login): add e2e tests for oidc client central login

---
+++ javascript/central-login-oidc-new/e2e/utils/demo-users.ts
@@ -0,0 +1,13 @@
+/*
+ *
+ * Copyright © 2025 Ping Identity Corporation. All right reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license. See the LICENSE file for details.
+ *
+ */
+
+export const pingAmUsername = 'sdkuser';
+export const pingAmPassword = 'password';
+export const pingOneUsername = 'demouser';
+export const pingOnePassword = 'U.QPDWEN47ZMyJhCDmhGLK*nr';
