fa6a5d7d109881cee3a13f3adc27a63ad605719e
George Bafaloukas <george.bafaloukas@forgerock.com>
2026-01-07 11:31:26 +0000

    Addressing review comments

--- iOS/swiftui-journey-module/JourneyModuleSample/JourneyModuleSample/Callbacks/CustomPinCollector.swift
+++ iOS/swiftui-journey-module/JourneyModuleSample/JourneyModuleSample/Callbacks/CustomPinCollector.swift
@@ -1,3 +1,13 @@
+//
+//  CustomPinCollector.swift
+//  JourneyModuleSample
+//
+//  Copyright (c) 2026 Ping Identity Corporation. All rights reserved.
+//
+//  This software may be modified and distributed under the terms
+//  of the MIT license. See the LICENSE file for details.
+//
+
 import Foundation
 import UIKit
 import SwiftUI
--- iOS/swiftui-journey-module/JourneyModuleSample/JourneyModuleSample/Callbacks/DeviceSigningVerifierCallbackView.swift
+++ iOS/swiftui-journey-module/JourneyModuleSample/JourneyModuleSample/Callbacks/DeviceSigningVerifierCallbackView.swift
@@ -1,3 +1,12 @@
+//
+//  DeviceSigningVerifierCallbackView.swift
+//  JourneyModuleSample
+//
+//  Copyright (c) 2026 Ping Identity Corporation. All rights reserved.
+//
+//  This software may be modified and distributed under the terms
+//  of the MIT license. See the LICENSE file for details.
+//
 
 import SwiftUI
 import PingBinding
