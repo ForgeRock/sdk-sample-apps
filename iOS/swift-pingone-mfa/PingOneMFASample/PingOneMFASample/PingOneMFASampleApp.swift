17ccb918a8ff196382700a0b2bca7455238e03da
Giora Krasilshchik <giorakrasilshchik@pingidentity.com>
2026-06-03 12:09:44 +0300

    sample app skeleton

---
+++ iOS/swift-pingone-mfa/PingOneMFASample/PingOneMFASample/PingOneMFASampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PingOneMFASampleApp.swift
+//  PingOneMFASample
+//
+//  Created by Giora Krasilshchik on 03/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct PingOneMFASampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
