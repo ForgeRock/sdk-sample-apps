50dbcf7a7c3e63a524932f4b64401d6da03bb710
AJ Ancheta <7781450+ancheetah@users.noreply.github.com>
2026-06-16 12:57:32 -0400

    chore: update davinci password in JS e2e

--- javascript/reactjs-todo-davinci/e2e/utils/demo-user.js
+++ javascript/reactjs-todo-davinci/e2e/utils/demo-user.js
@@ -6,5 +6,5 @@
  *
  */
 export const username = 'JsDvSampleAppsE2E@user.com';
-export const password = 'Demo_12345!';
+export const password = 'DemoPassword#123';
 export const displayName = 'JS DaVinci Sample Apps E2E';
