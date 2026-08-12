5e33ed0528b6df71f557d5322a389ee547985a35
AJ Ancheta <7781450+ancheetah@users.noreply.github.com>
2026-08-12 09:37:18 -0400

    test: improve protect e2e tests

--- javascript/reactjs-todo-journey/e2e/protect.spec.js
+++ javascript/reactjs-todo-journey/e2e/protect.spec.js
@@ -11,6 +11,8 @@
 import { test, expect } from '@playwright/test';
 import { password, username } from './utils/demo-user';
 
+const authenticateRequestUrl = 'https://openam-sdks.forgeblocks.com/am/json/alpha/authenticate';
+
 test.describe('React Journey - Login with Protect', () => {
   test('should succeed when initialized by callback', async ({ page }) => {
     const logs = [];
@@ -19,17 +21,49 @@ test.describe('React Journey - Login with Protect', () => {
       return Promise.resolve(true);
     });
 
+    let riskData;
+    page.on('request', (request) => {
+      const method = request.method();
+      const requestUrl = request.url();
+      const payload = request.postDataJSON();
+
+      // Only process POST requests with JSON payloads
+      if (method === 'POST' && payload && requestUrl.includes(authenticateRequestUrl)) {
+        const callback = payload.callbacks?.find(
+          (callback) => callback.type === 'PingOneProtectEvaluationCallback',
+        );
+
+        if (callback) {
+          const data = callback.input?.find((input) => input.name === 'IDToken1signals')?.value;
+          riskData = data;
+        }
+      }
+    });
+
     await page.goto('http://localhost:8443/?journey=TEST_Protect&initProtect=journey');
     await page.getByRole('link', { name: 'Sign In', exact: true }).click();
 
-    await page.getByLabel('User Name').fill(username);
-    await page.getByLabel('Password').fill(password);
-    await page.getByLabel('Password').press('Enter');
+    await expect(page.getByRole('heading', { name: 'Sign In' })).toBeVisible();
 
-    await page.waitForRequest('https://openam-sdks.forgeblocks.com/am/json/alpha/authenticate');
+    const protectPromise = page.waitForRequest((req) => {
+      return (
+        req.method() === 'POST' &&
+        req.url() === authenticateRequestUrl &&
+        req
+          .postDataJSON()
+          ?.callbacks?.some((callback) => callback.type === 'PingOneProtectEvaluationCallback')
+      );
+    });
+    await page.getByRole('textbox', { name: 'User Name' }).fill(username);
+    await page.getByRole('textbox', { name: 'Password' }).fill(password);
+    await page.getByRole('button', { name: 'Sign In' }).click();
+    await protectPromise;
 
-    await expect(logs.includes('Protect initialized by callback for data collection')).toBeTruthy();
-    await expect(logs.includes('Data set on Protect evaluation callback')).toBeTruthy();
+    expect(riskData).toBeDefined();
+    expect(riskData).toMatch(/^R\/o\//);
+
+    expect(logs.includes('Protect initialized by callback for data collection')).toBeTruthy();
+    expect(logs.includes('Data set on Protect evaluation callback')).toBeTruthy();
 
     page.removeListener('console', (msg) => console.log(msg.text()));
   });
@@ -41,19 +75,49 @@ test.describe('React Journey - Login with Protect', () => {
       return Promise.resolve(true);
     });
 
+    let riskData;
+    page.on('request', (request) => {
+      const method = request.method();
+      const requestUrl = request.url();
+      const payload = request.postDataJSON();
+
+      // Only process POST requests with JSON payloads
+      if (method === 'POST' && payload && requestUrl.includes(authenticateRequestUrl)) {
+        const callback = payload.callbacks?.find(
+          (callback) => callback.type === 'PingOneProtectEvaluationCallback',
+        );
+
+        if (callback) {
+          const data = callback.input?.find((input) => input.name === 'IDToken1signals')?.value;
+          riskData = data;
+        }
+      }
+    });
+
     await page.goto('http://localhost:8443/?journey=TEST_Protect&initProtect=bootstrap');
     await page.getByRole('link', { name: 'Sign In', exact: true }).click();
 
-    await page.getByLabel('User Name').fill(username);
-    await page.getByLabel('Password').fill(password);
-    await page.getByLabel('Password').press('Enter');
+    await expect(page.getByRole('heading', { name: 'Sign In' })).toBeVisible();
 
-    await page.waitForRequest('https://openam-sdks.forgeblocks.com/am/json/alpha/authenticate');
-    
-    await expect(
-      logs.includes('Protect initialized at bootstrap for data collection'),
-    ).toBeTruthy();
-    await expect(logs.includes('Data set on Protect evaluation callback')).toBeTruthy();
+    const protectPromise = page.waitForRequest((req) => {
+      return (
+        req.method() === 'POST' &&
+        req.url() === authenticateRequestUrl &&
+        req
+          .postDataJSON()
+          ?.callbacks?.some((callback) => callback.type === 'PingOneProtectEvaluationCallback')
+      );
+    });
+    await page.getByRole('textbox', { name: 'User Name' }).fill(username);
+    await page.getByRole('textbox', { name: 'Password' }).fill(password);
+    await page.getByRole('button', { name: 'Sign In' }).click();
+    await protectPromise;
+
+    expect(riskData).toBeDefined();
+    expect(riskData).toMatch(/^R\/o\//);
+
+    expect(logs.includes('Protect initialized at bootstrap for data collection')).toBeTruthy();
+    expect(logs.includes('Data set on Protect evaluation callback')).toBeTruthy();
 
     page.removeListener('console', (msg) => console.log(msg.text()));
   });
